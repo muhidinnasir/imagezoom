e57df6c0d863ce5ce2f50098f70567c7d2a4b0a3
muhidinnasir <muhidinnasir48@gmail.com>
2023-06-16 00:29:49 -0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/image_zoom/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.image_zoom
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Image Zoom'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: InteractiveViewer(
+          maxScale: 15.0,
+          minScale: 0.01,
+          boundaryMargin: const EdgeInsets.all(19),
+          child: Image.asset('assets/muhi.jpg'),
+        ),
+      ),
+    );
+  }
+}
